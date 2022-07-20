96764249a04b971f8bb032724bb4d84b9d700c86
rishi.tamrakar@uplight.com <rishi.tamrakar@uplight.com>
2022-07-20 11:51:32 +0000

    added sample code
    added sample code

--- sample-notebook-test-1.py
+++ sample-notebook-test-1.py
@@ -1,2 +1,12 @@
 # Databricks notebook source
+print("hello")
+
+
+
+df = spark.read.format("csv").load("s3://uplight-oe-sample-stg-us/sample_data.csv")
+df.show(10, False)
+print(df.count())
+df.show(1, False)
+
+df.write.format('csv').save("s3://uplight-oe-sample-stg-us/output_data_run_2.csv")
 
